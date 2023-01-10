1cda6a50e543f664d14477e5b67d9df9735b1242
AndreSilva <asilva@t-scape.com>
2023-01-10 16:37:23 +0000

    Adds graphs to step/channel stats

--- components/pipelines/StepsModal.tsx
+++ components/pipelines/StepsModal.tsx
@@ -5,7 +5,18 @@ import StepsForm from "./StepsForm";
 import {FiSettings} from "react-icons/fi";
 import SystemConfigModal from "./SystemConfigModal";
 import Button from "../layout/Button";
-import {trimNumber, successToToast, errorToToast} from "../graphs/utils/GraphUtils";
+import {trimNumber, successToToast, errorToToast, GraphData, handleArraySize} from "../graphs/utils/GraphUtils";
+import {Line} from "react-chartjs-2";
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip
+} from "chart.js";
 
 type StepProps = {
   connection: Connection,
@@ -56,8 +67,37 @@ export type QueueState = {
   bufferSize: number
 }
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  elements: {
+    line: {
+      borderWidth: 1
+    },
+    point: {
+      radius: 2
+    }
+  },
+  plugins: {
+    legend: {
+      position: 'bottom' as const,
+    },
+  },
+};
+
 export default function StepsModal({connection, step, isEdge, status}: StepProps) {
   const [stats, setStats] = useState({} as Statistics)
+  const [graphStats, setGraphStats] = useState([] as any)
   const [queueStatistics, setQueueStatistics] = useState({} as QueueStatistics)
   const [state, setState] = useState({} as StepState)
   const [jobState, setJobState] = useState({} as JobState)
@@ -86,6 +126,7 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
         const json = JSON.parse(event.data)
         if (json) {
           setStats(json)
+          setGraphStats((old: any) => [...handleArraySize(old), json])
         }
       }
 
@@ -113,6 +154,7 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
       const json = JSON.parse(event.data)
       if (json) {
         setQueueStatistics(json)
+        setGraphStats((old: any) => [...handleArraySize(old), json])
       }
     }
 
@@ -150,8 +192,8 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
             timeUnit: data.timeUnit
           })
         })
-          .then(response => successToToast(response))
-          .catch(response => errorToToast(response))
+        .then(response => successToToast(response))
+        .catch(response => errorToToast(response))
     }
     if (scheduleModal) {
       fetch(`${linkPrefix}://${connection?.address}/sn0wst0rm/api/1/jobs/${step.label}/schedule`,
@@ -162,8 +204,8 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
             schedule: data.cronJobPattern,
           })
         })
-          .then(response => successToToast(response))
-          .catch(response => errorToToast(response))
+        .then(response => successToToast(response))
+        .catch(response => errorToToast(response))
     }
     if (testModal) {
       fetch(`${linkPrefix}://${connection?.address}/sn0wst0rm/api/1/steps/${step.label}/test`,
@@ -198,8 +240,8 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
             bufferSize: parseInt(data.numericValue, 10),
           })
         })
-          .then(response => successToToast(response))
-          .catch(response => errorToToast(response))
+        .then(response => successToToast(response))
+        .catch(response => errorToToast(response))
     }
 
   }
@@ -233,8 +275,8 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
     fetch(`${linkPrefix}://${connection?.address}/sn0wst0rm/api/1/jobs/${step.label}/trigger`,
       {method: 'GET', headers: {'content-type': 'application/json'}}
     )
-        .then(response => successToToast(response))
-        .catch(response => errorToToast(response))
+      .then(response => successToToast(response))
+      .catch(response => errorToToast(response))
   }
 
   const handleOnSchedule = () => {
@@ -247,8 +289,8 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
     fetch(`${linkPrefix}://${connection?.address}/sn0wst0rm/api/1/steps/${step.label}/start`,
       {method: 'PUT', headers: {'content-type': 'application/json'}}
     )
-        .then(response => successToToast(response))
-        .catch(response => errorToToast(response))
+      .then(response => successToToast(response))
+      .catch(response => errorToToast(response))
 
   }
 
@@ -256,8 +298,8 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
     fetch(`${linkPrefix}://${connection?.address}/sn0wst0rm/api/1/steps/${step.label}/stop`,
       {method: 'PUT', headers: {'content-type': 'application/json'}}
     )
-        .then(response => successToToast(response))
-        .catch(response => errorToToast(response))
+      .then(response => successToToast(response))
+      .catch(response => errorToToast(response))
   }
 
   const handleOnBufferSize = () => {
@@ -267,43 +309,128 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
   }
 
   if (isEdge) {
-    return (<div className={'flex flex-col text-white gap-5'}>
-      <span>Put Statistics</span>
-      <div className={'flex gap-5 text-light-blue'}>
-        <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Mean Execution Time(ms): {trimNumber(queueStatistics?.put?.meanExecutionTimeMs)}</span>
-        <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Median Execution Time(ms): {trimNumber(queueStatistics?.put?.medianExecutionTimeMs)}</span>
-        <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Standard Deviation Execution Time: {trimNumber(queueStatistics?.put?.standardDeviationExecutionTimeMs)}</span>
-        <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Puts: {queueState.puts}</span>
-      </div>
-      <span>Take Statistics</span>
-      <div className={'flex gap-5 text-light-blue'}>
-        <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Mean Execution Time(ms): {trimNumber(queueStatistics?.take?.meanExecutionTimeMs)}</span>
-        <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Median Execution Time(ms): {trimNumber(queueStatistics?.take?.medianExecutionTimeMs)}</span>
-        <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Standard Deviation Execution Time: {trimNumber(queueStatistics?.take?.standardDeviationExecutionTimeMs)}</span>
-        <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Takes: {queueState.takes}</span>
-      </div>
-      <Button styles={'bg-light-blue'} onClick={() => handleOnBufferSize()}>Set Buffer Size</Button>
-      {
-        openModal && bufferSizeModal &&
-          <Modal onClose={handleClose} title={modalTitle} open={openModal} noOverlayClick={true}>
-              <StepsForm modalType={'buffer'} data={queueState.bufferSize} onSubmit={onSubmit} stepType={step.type} status={queueState.closed || status === 'online'}/>
-          </Modal>
-      }
-    </div>)
+    return (
+      <div className={'flex flex-col text-white gap-5'}>
+        <div className={'flex flex-col gap-5 text-light-blue'}>
+          <div className="flex h-96">
+            <div className='w-full h-full'>
+              <Line
+                options={{
+                ...options,
+                plugins: {...options.plugins, title: {display: true, text: 'Put Statistics'}}
+              }}
+                    updateMode={'resize'}
+                    data={{
+                      labels: graphStats.map((item: GraphData) => new Date(item.timestamp).toLocaleString().split(', ')[1]),
+                      datasets: [{
+                        label: 'Mean Execution Time(ms)',
+                        data: graphStats.map((item: any) => trimNumber(item.put?.meanExecutionTimeMs)),
+                        borderColor: 'orange',
+                        backgroundColor: 'orange'
+                      },
+                        {
+                          label: 'Median Execution Time(ms)',
+                          data: graphStats.map((item: any) => trimNumber(item.put?.medianExecutionTimeMs)),
+                          borderColor: 'green',
+                          backgroundColor: 'green'
+                        },
+                        {
+                          label: 'Standard Deviation Execution Time',
+                          data: graphStats.map((item: any) => trimNumber(item.put?.standardDeviationExecutionTimeMs)),
+                          borderColor: 'lightBlue',
+                          backgroundColor: 'lightBlue'
+                        }]
+                    }}
+              />
+            </div>
+
+            <div className='w-full h-full'>
+              <Line options={{
+                ...options,
+                plugins: {...options.plugins, title: {display: true, text: 'Take Statistics'}}
+              }}
+                    updateMode={'resize'}
+                    data={{
+                      labels: graphStats.map((item: GraphData) => new Date(item.timestamp).toLocaleString().split(', ')[1]),
+                      datasets: [{
+                        label: 'Mean Execution Time(ms)',
+                        data: graphStats.map((item: any) => trimNumber(item.take?.meanExecutionTimeMs)),
+                        borderColor: 'orange',
+                        backgroundColor: 'orange'
+                      },
+                        {
+                          label: 'Median Execution Time(ms)',
+                          data: graphStats.map((item: any) => trimNumber(item.take?.medianExecutionTimeMs)),
+                          borderColor: 'green',
+                          backgroundColor: 'green'
+                        },
+                        {
+                          label: 'Standard Deviation Execution Time',
+                          data: graphStats.map((item: any) => trimNumber(item.take?.standardDeviationExecutionTimeMs)),
+                          borderColor: 'lightBlue',
+                          backgroundColor: 'lightBlue'
+                        }]
+                    }}
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-between">
+            <span
+              className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Puts: {queueState.puts}</span>
+            <span
+              className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Takes: {queueState.takes}</span>
+          </div>
+        </div>
+
+        <Button styles={'bg-light-blue'} onClick={() => handleOnBufferSize()}>Set Buffer Size</Button>
+        {
+          openModal && bufferSizeModal &&
+            <Modal onClose={handleClose} title={modalTitle} open={openModal} noOverlayClick={true}>
+                <StepsForm modalType={'buffer'} data={queueState.bufferSize} onSubmit={onSubmit} stepType={step.type}
+                           status={queueState.closed || status === 'online'}/>
+            </Modal>
+        }
+      </div>)
   }
 
   return (
     <div className={'flex flex-col gap-5'}>
-      <div className={'flex gap-5 justify-between text-light-blue'}>
-        <div className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/4'}>Mean Execution Time(ms): {trimNumber(stats.meanExecutionTimeMs)}</div>
-        <div className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/4'}>Median Execution Time(ms): {trimNumber(stats.medianExecutionTimeMs)}</div>
-        <div className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/4'}>Standard Deviation Execution Time: {trimNumber(stats.standardDeviationExecutionTimeMs)}</div>
+      <div className='w-full h-96'>
+        <Line options={{
+          ...options,
+          plugins: {...options.plugins, title: {display: true, text: 'Take Statistics'}}
+        }}
+              updateMode={'resize'}
+              data={{
+                labels: graphStats.map((item: GraphData) => new Date(item.timestamp).toLocaleString().split(', ')[1]),
+                datasets: [{
+                  label: 'Mean Execution Time(ms)',
+                  data: graphStats.map((item: any) => trimNumber(item.meanExecutionTimeMs)),
+                  borderColor: 'orange',
+                  backgroundColor: 'orange'
+                },
+                  {
+                    label: 'Median Execution Time(ms)',
+                    data: graphStats.map((item: any) => trimNumber(item.medianExecutionTimeMs)),
+                    borderColor: 'green',
+                    backgroundColor: 'green'
+                  },
+                  {
+                    label: 'Standard Deviation Execution Time',
+                    data: graphStats.map((item: any) => trimNumber(item.standardDeviationExecutionTimeMs)),
+                    borderColor: 'lightBlue',
+                    backgroundColor: 'lightBlue'
+                  }]
+              }}
+        />
       </div>
       {
         step?.type !== 'job' &&
           <>
               <div className={'flex gap-5 justify-between text-light-blue'}>
-                  <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/4'}>Total Calls: {state.totalStepCalls}</span>
+                  <span
+                      className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/4'}>Total Calls: {state.totalStepCalls}</span>
                   <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/4'}>Successful Calls: {state.successfulStepCalls}</span>
                   <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/4'}>Unsuccessful Calls: {state.unsuccessfulStepCalls}</span>
               </div>
@@ -311,9 +438,11 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
                   <Button styles={'bg-light-blue'} onClick={() => handleOnTest()}>Test</Button>
                   <Button styles={'bg-light-blue'} onClick={() => handleOnThreads()}>Threads</Button>
                   <Button styles={'bg-light-blue'} onClick={() => handleOnPollFrequency()}>Poll Frequency</Button>
-                  <Button styles={'bg-light-blue'} disabled={!state.stopped} onClick={() => handleStartStep()}>Start</Button>
-                  <Button styles={'bg-light-blue'} disabled={state.stopped} onClick={() => handleStopStep()}>Stop</Button>
-                  <Button styles={'bg-light-blue font-bold text-16'} >
+                  <Button styles={'bg-light-blue'} disabled={!state.stopped}
+                          onClick={() => handleStartStep()}>Start</Button>
+                  <Button styles={'bg-light-blue'} disabled={state.stopped}
+                          onClick={() => handleStopStep()}>Stop</Button>
+                  <Button styles={'bg-light-blue font-bold text-16'}>
                       <FiSettings onClick={() => setConfigurationsModal(true)}/>
                   </Button>
                 {
@@ -333,17 +462,22 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
         step.type === 'job' &&
           <>
               <div className={'flex gap-5 text-light-blue'}>
-                  <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Last Trigger: {jobState.lastTrigger}</span>
+                  <span
+                      className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Last Trigger: {jobState.lastTrigger}</span>
                   <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Last Running Attempt: {jobState.lastAttempt}</span>
-                  <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Triggered: {jobState.triggered}</span>
-                  <span className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Status: {jobState.status}</span>
+                  <span
+                      className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Triggered: {jobState.triggered}</span>
+                  <span
+                      className={'flex gap-10 bg-card p-3 border border-solid border-gray-400 w-1/5'}>Status: {jobState.status}</span>
               </div>
               <div className={'flex bg-card p-3 border border-solid border-gray-400'}>
                   <Button styles={'bg-light-blue'} onClick={() => handleOnTrigger()}>Trigger</Button>
                   <Button styles={'bg-light-blue'} onClick={() => handleOnSchedule()}>Schedule</Button>
-                  <Button styles={'bg-light-blue'} disabled={!jobState.stopped} onClick={() => handleStartStep()}>Start</Button>
-                  <Button styles={'bg-light-blue'} disabled={jobState.stopped} onClick={() => handleStopStep()}>Stop</Button>
-                  <Button styles={'bg-light-blue font-bold text-16'} >
+                  <Button styles={'bg-light-blue'} disabled={!jobState.stopped}
+                          onClick={() => handleStartStep()}>Start</Button>
+                  <Button styles={'bg-light-blue'} disabled={jobState.stopped}
+                          onClick={() => handleStopStep()}>Stop</Button>
+                  <Button styles={'bg-light-blue font-bold text-16'}>
                       <FiSettings onClick={() => setConfigurationsModal(true)}/>
                   </Button>
                 {
@@ -364,19 +498,23 @@ export default function StepsModal({connection, step, isEdge, status}: StepProps
           <Modal onClose={handleClose} title={modalTitle} open={openModal} noOverlayClick={true}>
             {
               threadModal &&
-                <StepsForm modalType={'thread'} data={state} onSubmit={onSubmit} stepType={step.type} status={status === 'online'}/>
+                <StepsForm modalType={'thread'} data={state} onSubmit={onSubmit} stepType={step.type}
+                           status={status === 'online'}/>
             }
             {
               pollFrequencyModal &&
-                <StepsForm modalType={'pollFrequency'} data={state} onSubmit={onSubmit} stepType={step.type} status={status === 'online'}/>
+                <StepsForm modalType={'pollFrequency'} data={state} onSubmit={onSubmit} stepType={step.type}
+                           status={status === 'online'}/>
             }
             {
               scheduleModal &&
-                <StepsForm modalType={'schedule'} data={jobState} onSubmit={onSubmit} stepType={step.type} status={status === 'online'}/>
+                <StepsForm modalType={'schedule'} data={jobState} onSubmit={onSubmit} stepType={step.type}
+                           status={status === 'online'}/>
             }
             {
               testModal &&
-                <StepsForm modalType={'test'} data={testResult} onSubmit={onSubmit} stepType={step.type} status={status === 'online'}/>
+                <StepsForm modalType={'test'} data={testResult} onSubmit={onSubmit} stepType={step.type}
+                           status={status === 'online'}/>
             }
           </Modal>
       }
