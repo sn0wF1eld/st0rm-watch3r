e6b243c2c214571a047442dfb789af4cf56533f0
AgarDraug <andre.2107.silva@gmail.com>
2025-07-03 15:37:58 +0100

    Adds Replay From Step Component

--- components/pipelines/ReplayFromStepComponent.tsx
+++ components/pipelines/ReplayFromStepComponent.tsx
@@ -11,12 +11,13 @@ export default function ReplayFromStepComponent(props: Props) {
   const {connection} = useContextProvider()
   const [steps, setSteps] = useState(['']);
   useEffect(() => {
+    if (!connection) return
     fetch(`${connection?.secure ? 'https' : 'http'}://${connection?.address}/sn0wst0rm/api/1/pipelines/${props.pipelineId}/transactions/failed/${props.txId}/shards/${props.shardId}/steps`,
       {method: 'GET', headers: {'content-type': 'application/json'}}
     ).then(res => res.json()
       .then((body: string[]) => {
         return setSteps(body)
       }))
-  }, []);
+  }, [connection]);
   return <div>{JSON.stringify(steps)}</div>
 }
